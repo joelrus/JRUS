c4a0e02a99573935e5a9e14f2b3e5fbc4f306a99
joelrus <joelusuari1@gmail.com>
2016-10-04 21:05:12 +0200

    v1.P2_Import hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello World: To forgive all is as inhuman(aristotil)");
+        System.out.println("Hello World: To forgive all is as inhuman(seneca)");
     }
 
 }
